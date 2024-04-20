b7da2b3002b568e537000ddedef34b4e01161c94
Nicolas Petras <nicolaspetras@gmail.com>
2024-04-20 20:21:34 +0100

    feat(caching): add database drivers for platforms and interface

---
+++ SpaceTutorial/shared/src/commonMain/kotlin/com/jetbrains/spacetutorial/cache/Database.kt
@@ -0,0 +1,6 @@
+package com.jetbrains.spacetutorial.cache
+
+internal class Database(databaseDriverFactory: DatabaseDriverFactory) {
+    private val database = AppDatabase(databaseDriverFactory.createDriver())
+    private val dbQuery = database.appDatabaseQueries
+}
